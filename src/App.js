a2cc3bce6d6efff0c2157b98dd1c2ec0727718ca
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-01-29 21:55:43 -0500

    updated App.js with imports and paths to files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import AboutMe from "./components/pages/AboutMe";
+import Portfolio from "./components/pages/Portfolio";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router basename="/">
+      <Navbar />
+      <Route path="/" exact component={Home} />
+      <Route path="/aboutme" component={AboutMe} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route
+        path="/github"
+        component={() => {
+          window.location.href = "https://github.com/Samuel-en";
+          return null;
+        }}
+      />
+        <Route
+          path="/linkedin"
+          component={() => {
+            window.location.href =
+              "https://www.linkedin.com/in/samuel-endrias-098292121/";
+            return null;
+          }}
+        />
+        <Footer />
+      </Router>
+    </>
   );
 }
+
 
 export default App;
