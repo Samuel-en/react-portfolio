17ecd910b53c03b7dfaaf369ff0c7cce58fb0ee3
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-01-30 19:10:29 -0500

    footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            &copy;Copyright 2021 Samuel-en
+        </footer>
+    )
+}
+
+export default Footer
